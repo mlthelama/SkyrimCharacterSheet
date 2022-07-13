031af917fda839fbafd9e47432fa5a7c4ab266d3
mlthelama <lackner.martin79@gmail.com>
2022-07-13 02:59:07 +0200

    fixes after main merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include "events.h"
 #include "scaleform/scaleform.h"
 
-void InitLogger() {
-    static bool initialized = false;
-    if (!initialized) {
+void init_logger() {
+    if (static bool initialized = false; !initialized) {
         initialized = true;
     } else {
         return;
@@ -31,26 +30,26 @@ void InitLogger() {
         log->flush_on(spdlog::level::trace);
 #endif
 
-        spdlog::set_default_logger(move(log));
+        set_default_logger(move(log));
         spdlog::set_pattern("[%H:%M:%S.%f] %s(%#) [%^%l%$] %v"s);
 
         logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
 
         try {
-            Settings::load();
+            settings::load();
             logger::info("Settings Loaded"sv);
         } catch (const std::exception& e) { logger::warn("failed to load setting {}"sv, e.what()); }
 
-        switch (*Settings::logLevel) {
-            case _constLogTrace:
+        switch (*settings::log_level) {
+            case const_log_trace:
                 spdlog::set_level(spdlog::level::trace);
                 spdlog::flush_on(spdlog::level::trace);
                 break;
-            case _constLogDebug:
+            case const_log_debug:
                 spdlog::set_level(spdlog::level::debug);
                 spdlog::flush_on(spdlog::level::debug);
                 break;
-            case _constLogInfo:
+            case const_log_info:
                 spdlog::set_level(spdlog::level::info);
                 spdlog::flush_on(spdlog::level::info);
                 break;
@@ -60,7 +59,7 @@ void InitLogger() {
 
 extern "C" DLLEXPORT constexpr auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v{};
-    v.PluginVersion(Version::ASINT);
+    v.PluginVersion(Version::MAJOR);
     v.PluginName(Version::PROJECT.data());
     v.AuthorName(Version::AUTHOR);
     v.UsesAddressLibrary(true);
@@ -70,18 +69,18 @@ extern "C" DLLEXPORT constexpr auto SKSEPlugin_Version = []() {
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
     try {
-        InitLogger();
+        init_logger();
 
         logger::info("ShowStats loading"sv);
 
-        SKSE::Init(a_skse);
+        Init(a_skse);
 
         SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
             switch (a_msg->type) {
                 case SKSE::MessagingInterface::kDataLoaded:
                     logger::info("Data loaded"sv);
-                    Events::SinkEventHandlers();
-                    Scaleform::Register();
+                    events::sink_event_handlers();
+                    scaleform::Register();
                     break;
             }
         });
