3372650fcb21e4160b645786f6891aa5daeb3d4c
mlthelama <47679057+mlthelama@users.noreply.github.com>
2022-12-21 12:51:49 +0100

    fix so it runs on the newest version

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,8 @@ EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Versio
     v.AuthorName(Version::AUTHOR);
     v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH, Version::BETA });
     v.UsesAddressLibrary(true);
-    v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST_SE });
+    v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
+    v.HasNoStructUse(true);
     return v;
 }();
 
