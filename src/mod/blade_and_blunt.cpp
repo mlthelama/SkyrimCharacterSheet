cbf108cf90877ac426b8ab8b11649f88dea561bd
mlthelama <47679057+mlthelama@users.noreply.github.com>
2024-01-12 18:40:36 +0100

    `fix` #38
    * corrected bb calculation

--- src/mod/blade_and_blunt.cpp
+++ src/mod/blade_and_blunt.cpp
@@ -4,16 +4,18 @@
 namespace mod {
     float blade_and_blunt::calculate_armor_damage_resistance(float a_vanilla) {
         const auto game_settings = setting::game_setting::get_singleton();
+        auto vanilla = a_vanilla / 100;
         auto factor = game_settings->get_armor_scaling_factor();
-        auto armor_rating = (a_vanilla / factor) * 100;
+        auto armor_rating = (vanilla / factor) * 100;
 
         if (armor_rating <= 500) {
-            return a_vanilla;
+            return vanilla * 100;
         } else if (armor_rating < 1000) {
             auto remainderRating = armor_rating - 500;
-            return 0.75f + (remainderRating / 100 * game_settings->get_armor_base_factor());
+            //bb sets base factor to 0
+            return (0.75f + (remainderRating / 100 * 0.03f)) * 100;
         } else {
-            return 0.90f;
+            return 0.90f * 100;
         }
 
     }  // mod
