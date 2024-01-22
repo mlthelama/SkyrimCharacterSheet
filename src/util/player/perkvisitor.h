3dba95fab8c85c8305291f234463f4f196aaca40
mlthelama <mlthelama@users.noreply.github.com>
2024-01-22 23:43:11 +0000

    maintain

--- src/util/player/perkvisitor.h
+++ src/util/player/perkvisitor.h
@@ -2,15 +2,13 @@
 
 //TODO update clib
 namespace util {
-    class perk_visitor : public RE::PerkEntryVisitor
-    {
+    class perk_visitor : public RE::PerkEntryVisitor {
     public:
-        explicit perk_visitor(RE::Actor* a_actor, float a_base)
-        {
-            actor_  = a_actor;
+        explicit perk_visitor(RE::Actor* a_actor, float a_base) {
+            actor_ = a_actor;
             result_ = a_base;
         }
-        
+
         RE::BSContainer::ForEachResult Visit(RE::BGSPerkEntry* perk_entry) override;
 
         [[nodiscard]] float get_result() const;
